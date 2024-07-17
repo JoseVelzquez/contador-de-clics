fbddc959dd285f37066dc30679646a233956babd
JoseDev <josevelazquezolvera2@gmail.com>
2024-07-17 16:55:08 -0600

    nuevo_commit1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
-import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
-
 
 class App extends React.Component {
   constructor() {
@@ -26,13 +24,6 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className='freecodecamp-logo-contenedor'>
-          <img 
-          className='freecodecamp-logo'
-          src={freeCodeCampLogo}
-          alt='Logo de freecodecamp'/>
-          
-        </div>
         <div className='contenedor-principal'>
           <Contador numClics={this.state.numClics} />
           <Boton 
@@ -50,43 +41,5 @@ class App extends React.Component {
     );
   }
 }
-
-// function App() {
-
-//   const [numClics, setNumClics] = useState(0);
-
-//   const manejarClic = () => {
-//     setNumClics(numClics + 1);
-//   };
-
-//   const reiniciarContador = () => {
-//     setNumClics(0);
-//   };
-
-//   return (
-//     <div className="App">
-//       <div className='freecodecamp-logo-contenedor'>
-//         <img 
-//         className='freecodecamp-logo'
-//         src={freeCodeCampLogo}
-//         alt='Logo de freecodecamp'/>
-        
-//       </div>
-//       <div className='contenedor-principal'>
-//         <Contador numClics={numClics} />
-//         <Boton 
-//           texto='Clic'
-//           esBotonDeClic={true}
-//           manejarClic={manejarClic} />
-//         <Boton
-//           texto='Reiniciar'
-//           esBotonDeClic={false}
-//           manejarClic={reiniciarContador} />
-
-//       </div>
-      
-//     </div>
-//   );
-// }
 
 export default App;
